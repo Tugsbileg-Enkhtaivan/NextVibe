9dcef5b431855bdd7714528abc09eca17eb61095
Narangerel0121 <nrngrl0121@gmail.com>
2025-05-13 20:54:42 +0800

    nodejs & typescript setup

--- apps/backend/services/userService.ts
+++
@@ -1,5 +0,0 @@
-// import prisma from './prisma/client';
-
-// export const getUserProfile = async (id: string) => {
-//   return prisma.user.findUnique({ where: { id } });
-// };
--- apps/backend/utils/token.ts
+++
@@ -1,4 +0,0 @@
-export const generateToken = (userId: string) => {
-    // JWT logic here
-  };
-  

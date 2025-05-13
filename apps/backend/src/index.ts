380268bbaa79d7903cfff0c1351d7461bba5ce24
Narangerel0121 <nrngrl0121@gmail.com>
2025-05-13 21:48:56 +0800

    connected to neon

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello from Vibez")
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get('/', async(_req, res) => {
+    const users = await prisma.user.findMany()
+    res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const user = await prisma.user.create({
+      data: { name, email },
+    });
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

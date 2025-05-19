5179b7fea55354541893fa9d9f0c077b69e5dd0c
Narangerel0121 <nrngrl0121@gmail.com>
2025-05-19 08:25:24 +0800

    token

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -4,6 +4,8 @@ import { PrismaClient } from "@prisma/client";
 import cors from "cors";
 import aiSongRouter from "./routes/aiSongRouter";
 import spotifyRouter from "./routes/spotifyRouter";
+import { verifyToken } from "@clerk/backend";
+import { error } from "console";
 
 dotenv.config();
 
@@ -16,7 +18,22 @@ app.use(express.json());
 app.use("/api/ai-song-search", aiSongRouter);
 app.use("/api/spotify", spotifyRouter);
 
-app.get("/", async (_req, res) => {
+
+app.get("/", async (req, res) => {
+  const { authorization } = req.headers;
+  const token = authorization?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ error: "Unauthorzied" })
+    return;
+  }
+  try {
+    const verifyiedToken = await verifyToken(token, {
+      secretKey: process.env.CLERK_SECRET_KEY,
+    })
+  } catch (error) {
+    res.status(401).json({ error: "Unauthorzied" })
+  }
+
   const users = await prisma.user.findMany();
   res.json(users);
 });
@@ -27,7 +44,7 @@ app.post("/users", async (req, res) => {
     const user = await prisma.user.create({
       data: { username, email },
     });
-    res.json({message: "User created successfully", user});
+    res.json({ message: "User created successfully", user });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to create user" });

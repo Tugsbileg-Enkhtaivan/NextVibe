0c309e452ceb30b75ac1e016faad7f97adb81ed2
24LP6042 24LP6042 <24LP6042@G303-CC0148.local>
2025-05-21 11:22:28 +0800

    first history page commit

---
+++ apps/frontend/src/app/history/page.tsx
@@ -0,0 +1,9 @@
+export default function HistoryPage() {
+    return (
+        <section className="w-full h-screen">
+            <div className="min-w-[375px] w-[400px] min-h-[670px] bg-gray-100 mx-auto p-4 rounded-lg">
+                <h1 className="font-bold text-2xl">Watch History</h1>
+            </div>
+        </section>
+    )
+}
